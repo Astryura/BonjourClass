06f8557eb24e4373ac15186580341d7b39e36524
Astrid Hlavacek <astrid-hlavacek@hotmail.fr>
2016-11-22 14:42:05 +0100

    Exercice Hello World

---
+++ Bonjour.java
@@ -0,0 +1,17 @@
+
+/**
+*
+* TP1 class Bonjour
+* Exercice Hello World
+*
+*/
+
+public class Bonjour {  // Class Bonjour
+
+    public static void main(String args[]) {
+
+    System.out.println("Bonjour tout le monde"); // Affichage
+
+    }
+
+}
